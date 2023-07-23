adfc2406338be79bd6b624157aed0948ea71285b
Hesham <dev@heshamorabi.com>
2023-07-23 09:30:37 -0700

    UPDATE 6 -> Fix printf to handle exc

--- _printf.c
+++ _printf.c
@@ -3,37 +3,45 @@
 /**
  * _printf - myown implemintation of printf func
  * @format: str pointer
- * Return: length of char printed
+ * Return: length of char printed or -1 if there an error
 */
 int _printf(const char *format, ...)
 {
 	m_handle conv[] = {
 		{"%c", print_char}, {"%s", print_string}, {"%%", print_present}
 	};
-
 	va_list args;
 	int i, j, len = 0;
 
 	va_start(args, format);
-
-	if (format == NULL)
+	if (format == '\0' || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		for (j = 0; j < (sizeof(conv) / sizeof(conv[j])); j++)
+		if (format[i] == conv[0].module[0])
 		{
-			if (format[i] == conv[j].module[0] && format[i + 1] == conv[j].module[1])
+			for (j = 0; j < (sizeof(conv) / sizeof(conv[j])); j++)
 			{
-				len += conv[j].function(args);
-				i += 2;
-				continue;
+				if (format[i + 1] == conv[j].module[1])
+				{
+					len += conv[j].function(args);
+					i += 1;
+					break;
+				}
+			}
+			if (j == sizeof(conv) / sizeof(conv[j]))
+			{
+				va_end(args);
+				return (-1);
 			}
 		}
-		_putchar(format[i]);
-		len++;
+		else
+		{
+			_putchar(format[i]);
+			len++;
+		}
 	}
-
 	va_end(args);
 	return (len);
 }
