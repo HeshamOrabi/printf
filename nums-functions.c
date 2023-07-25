236b8b36d8af53c82505758180521dcc2f5fdc0c
KhaledAbdellaty <kabdellaty@gmail.com>
2023-07-25 09:44:05 -0700

    UPDATE 21 -> update print_int function

--- nums-functions.c
+++ nums-functions.c
@@ -1,64 +1,46 @@
 #include "main.h"
 
 /**
- * int_to_char - function that convert integer
- * to string
- * @num: an integer numer
- * Return: returns pointer to string
- */
-char *int_to_char(int num)
-{
-	int i = 0;
-	int is_negative = 0, digit, j;
-	char tempd;
-	char *str = (char *) malloc(sizeof(char));
-
-	if (num < 0)
-	{
-		is_negative = 1;
-		num = -num;
-	}
-	do {
-		digit = num % 10;
-		str[i++] = digit + '0';
-		num /= 10;
-	} while (num > 0);
-
-	if (is_negative)
-	{
-		str[i++] = '-';
-	}
-
-	for (j = 0; j < i / 2; j++)
-	{
-		tempd = str[j];
-		str[j] = str[i - j - 1];
-		str[i - j - 1] = tempd;
-	}
-
-	str[i] = '\0';
-
-	return (str);
-}
-
-/**
  * print_int - function that prints integers
  * @args: an argument list
  * Return: returns length of integrs
  */
 int print_int(va_list args)
 {
-	int num, i;
-	char *s;
+	int n, tmpn, tmp;
+	int lastn, len, countd;
 
-	num = va_arg(args, int);
+	n = va_arg(args, int);
+	lastn = n % 10;
+	n = n / 10;
+	tmpn = n;
+	len = 1;
 
-	s = int_to_char(num);
-
-	for (i = 0; s[i] != '\0'; i++)
-		_putchar(s[i]);
-
-	return (i);
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = -n;
+		lastn = -lastn;
+		tmpn = -tmpn;
+		len++;
+	}
+	countd = 1;
+	while (tmpn / 10)
+	{
+		countd = countd * 10;
+		tmpn = tmpn / 10;
+	}
+	tmpn = n;
+	while (tmpn > 0)
+	{
+		tmp = tmpn / countd;
+		_putchar(tmp + '0');
+		tmpn = tmpn - (tmp * countd);
+		countd /= 10;
+		len++;
+	}
+	_putchar(lastn + '0');
+	return (len);
 }
 
 /**
